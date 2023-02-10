9c87c13f9c46549471d64042dd4a77a807209c88
RAVI SINGH <ravisingh1996thakur@gmail.com>
2023-02-10 14:40:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "ML HOUSE REGRESSION "
+    return "ML HOUSE REGRESSION CALIFORNIA DATASET "
 
 
 if __name__=="__main__":
