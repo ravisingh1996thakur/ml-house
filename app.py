34ef529805cc901d6f714317201374a47baae483
RAVI SINGH <ravisingh@RAVIs-MacBook-Air.local>
2023-02-10 19:35:52 +0530

    files updated

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask
+import sys
+from housing.logger import logging 
+from housing.exception import HousingException
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    try:
+        raise Exception("we are esting ustum exception ")
+    except Exception as e:
+        housing= HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("we are testing logging module")
     return "ML HOUSE REGRESSION CALIFORNIA DATASET "
 
 
